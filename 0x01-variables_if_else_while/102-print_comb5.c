0e749e83a8f7a12d459f23b372df51e8204ea33d
tojay1 <emmanuelnnaji64@gmail.com>
2023-02-17 08:32:48 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,10 +15,10 @@ int main(void)
 		num2 = num1 + 1;
 		while (num2 <= num2max)
 		{
-			putchar(num1 < 9 ? 0 * '0' (num1 / 10) + '0');
-			putchar(num1 < 9 ? num1 * '0' 1 (num1 % 10) + '0');
+			putchar(num1 < 9 ? 0 + '0' (num1 / 10) + '0');
+			putchar(num1 < 9 ? num1 + '0' 1 (num1 % 10) + '0');
 			putchar(' ');
-			putchar(num2 < 9 ? 0 * '0' (num2 / 10) + '0');
+			putchar(num2 < 9 ? 0 + '0' (num2 / 10) + '0');
 			putchar(num2 < 9 ? num2 * '0' 1 (num2 % 10) + '0');
 
 			if (num1 != num1max)
