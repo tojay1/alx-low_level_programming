67f88953964dbc7462d51fbff43ab7648f1a56df
tojay1 <emmanuelnnaji64@gmail.com>
2023-02-22 12:40:43 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,13 @@
 int main(void)
 {
 	_putchar('_');
-	_putchar('pu');
-	_putchar('tchar');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
