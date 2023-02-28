5723f85ce0a1288d1f1dbe3bfe5db8a70ee43edb
tojay1 <emmanuelnnaji64@gmail.com>
2023-02-28 02:10:56 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+    int *p;
+
+    n = 402;
+    printf("n=%d\n", n);
+    *p = 98;
+    printf("n=%d\n", n);
+    return (0);
+}
